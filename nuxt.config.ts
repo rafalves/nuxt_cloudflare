5052bdc682c8ea1a4c9adc3b3b73a9274d77eff0
Rafael Alves <rafalves.costa@gmail.com>
2025-03-27 01:48:14 -0300

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxthub/core'],
-  nitro: {
-    preset: "cloudflare_module",
-
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
-    }
+  hub: {
+    database: true
   },
+  $development: {
+    hub: {
+      remote: true
+    }
+  }
 })
